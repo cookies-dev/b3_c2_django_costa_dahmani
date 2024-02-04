b9c59d0f8cd8c852639d2b4d1168aa2dffb4d75e
MaximCosta <mxmcosta@gmail.com>
2024-02-04 14:00:00 +0100

    feat: add tailwind && folder struct

--- b3_c2_django_costa_dahmani/urls.py
+++ b3_c2_django_costa_dahmani/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for password_manager project.
+URL configuration for b3_c2_django_costa_dahmani project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('password_manager.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
